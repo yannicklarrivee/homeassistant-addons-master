bf80045e0d54099ddf9fa842ee839d1ceac771b2
Yannick Larrivée <yannicklarrivee@gmail.com>
2021-11-27 13:40:31 -0500

    remove untracked files

--- tshark/local_build_and_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker build  -t local/tshark . \
-&& \
-docker run --privileged --net=host 
---rm -v /Users/yannicklarrivee/Dev/homeassistant-addons-master/tshark/test_data:/data local/tshark
