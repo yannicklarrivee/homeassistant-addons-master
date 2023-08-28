83a77f029fa5905c87ee46a05216c7db9c77877b
Yannick Larrivée <yannicklarrivee@gmail.com>
2023-08-28 10:36:42 -0400

    First working version of voip mms for home assistant.

--- voipms-mms/run.sh
+++ voipms-mms/run.sh
@@ -4,24 +4,14 @@
 # Author: Yannick Larrivée <yannicklarrivee@gmail.com>
 
 CONFIG_PATH=/data/options.json
-export PROVIDER="$(jq --raw-output '.provider' $CONFIG_PATH)"
-export CLIENT_ID="$(jq --raw-output '.clientid' $CONFIG_PATH)"
-export CLIENT_SECRET="$(jq --raw-output '.clientsecret' $CONFIG_PATH)"
-export EMAIL_DOMAIN="$(jq --raw-output '.emaildomain' $CONFIG_PATH)"
-export COOKIE_SECRET="$(jq --raw-output '.cookiesecret' $CONFIG_PATH)"
-export CALLBACK_URL="$(jq --raw-output '.callbackurl' $CONFIG_PATH)"
-export AUTHENTICATED_EMAILS_FILE="$(jq --raw-output '.authenticatedemailsfile' $CONFIG_PATH)"
-export PATH=/root/go/bin:$PATH 
+export API_USERNAME="$(jq --raw-output '.api_username' $CONFIG_PATH)"
+export API_PASSWORD="$(jq --raw-output '.api_password' $CONFIG_PATH)"
+export DID="$(jq --raw-output '.did' $CONFIG_PATH)"
 
-# Start the listener and enter an endless loop
-echo "Starting oauth2-proxy with parameters:"
-echo "Auth provider =" $PROVIDER
-echo "Client ID=" $CLIENT_ID
-echo "Client secret=" $CLIENT_SECRET
-echo "Email domain allowed=" "${EMAIL_DOMAIN}"
-echo "Cookie secret=" $COOKIE_SECRET
-echo "Callback url=" $CALLBACK_URL
-echo "Authenticated emails file=" $AUTHENTICATED_EMAILS_FILE
-
-#oauth2-proxy --client-id $CLIENT_ID --client-secret $CLIENT_SECRET --cookie-secret $COOKIE_SECRET  --email-domain $EMAIL_DOMAIN --provider $PROVIDER --http-address=0.0.0.0:4180 --redirect-url $CALLBACK_URL
-oauth2-proxy --client-id $CLIENT_ID --client-secret $CLIENT_SECRET --cookie-secret $COOKIE_SECRET  --authenticated-emails-file $AUTHENTICATED_EMAILS_FILE --provider $PROVIDER --http-address=0.0.0.0:4180 --redirect-url $CALLBACK_URL 
+echo "Sending SMS"
+while read -r msg; do
+    echo "$msg"
+    DESTINATION="$(echo "$msg" | jq --raw-output '.destination')"    
+    SMS_MESSAGE="$(echo "$msg" | jq --raw-output '.message')"
+    echo "Message: ${SMS_MESSAGE}" && python3 send-sms.py $API_USERNAME $API_PASSWORD $DID $DESTINATION "$SMS_MESSAGE"
+done < /dev/stdin
